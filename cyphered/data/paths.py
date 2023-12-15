9d7c225c7c21f10d1cb0c5c72ea47e8bcb8fb62a
Xale <alexev2401@gmail.com>
2023-12-15 19:20:21 +0300

    python: PATHS CLASS!

--- cyphered/data/paths.py
+++ cyphered/data/paths.py
@@ -1,10 +1,31 @@
 import os
 
 RESOURCES_PATH = os.path.join(os.getcwd(), 'resources')
-SAVES_PATH = os.path.join(os.getcwd(), 'saves')
-DATA_PATH = os.path.join(os.getcwd(), 'data')
+DATA_PATH = os.path.join(os.getcwd(), 'gamedata')
+SAVES_PATH = os.path.join(DATA_PATH, 'saves')
 
 FONTS_PATH = os.path.join(RESOURCES_PATH, 'fonts')
 SPRITES_PATH = IMAGES_PATH = os.path.join(RESOURCES_PATH, 'sprites')
 MUSIC_PATH = os.path.join(RESOURCES_PATH, 'music')
 
+
+class Path:
+    @classmethod
+    def music(cls, filename: str) -> str:
+        return os.path.join(MUSIC_PATH, f'{filename}.mp3')
+
+    @classmethod
+    def sprite(cls, filename: str) -> str:
+        return os.path.join(SPRITES_PATH, f'{filename}.png')
+
+    @classmethod
+    def image(cls, filename: str) -> str:
+        return cls.sprite(filename)
+
+    @classmethod
+    def save(cls, filename: str) -> str:
+        return os.path.join(SAVES_PATH, f'{filename}.json')
+
+    @classmethod
+    def settings(cls) -> str:
+        return os.path.join(DATA_PATH, 'settings.txt')
