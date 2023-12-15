af014f25ca6dc044edeaac5a8d21fad1cc3b626a
Xale <alexev2401@gmail.com>
2023-12-15 18:57:18 +0300

    python: paths vars

---
+++ cyphered/data/paths.py
@@ -0,0 +1,5 @@
+import os
+
+RESOURCES_PATH = os.path.join(os.getcwd(), 'resources')
+FONTS_PATH = os.path.join(RESOURCES_PATH, 'fonts')
+SPRITES_PATH = IMAGES_PATH = os.path.join(RESOURCES_PATH, 'sprites')
