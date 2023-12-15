a628c2b82b1b7c39138c3f1cd2309d82fa657374
Xale <alexev2401@gmail.com>
2023-12-15 19:28:17 +0300

    python: Paths class comments

--- cyphered/data/paths.py
+++ cyphered/data/paths.py
@@ -10,22 +10,33 @@ MUSIC_PATH = os.path.join(RESOURCES_PATH, 'music')
 
 
 class Path:
+    """Class containing methods for file paths generation"""
+    # Usage: Paths.music('music1')
     @classmethod
     def music(cls, filename: str) -> str:
+        """Returns a path of MUSIC MP3 file by its filename."""
         return os.path.join(MUSIC_PATH, f'{filename}.mp3')
 
+    # Usage: Paths.sprite('sprite1')
     @classmethod
     def sprite(cls, filename: str) -> str:
+        """Returns a path of SPRITE PNG file by its filename."""
         return os.path.join(SPRITES_PATH, f'{filename}.png')
 
+    # Usage: Paths.image('sprite1')
     @classmethod
     def image(cls, filename: str) -> str:
+        """Returns a path of SPRITE PNG file by its filename. Equals to Paths.sprite(filename)"""
         return cls.sprite(filename)
 
+    # Usage: Paths.save('save1')
     @classmethod
     def save(cls, filename: str) -> str:
+        """Returns a path of SAVE TXT file by its filename."""
         return os.path.join(SAVES_PATH, f'{filename}.json')
 
+    # Usage: Paths.settings()
     @classmethod
     def settings(cls) -> str:
+        """Returns a path of settings TXT file."""
         return os.path.join(DATA_PATH, 'settings.txt')
