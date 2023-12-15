f700565bef4778ef395bef770e6a335b1d059113
Xale <alexev2401@gmail.com>
2023-12-15 19:15:32 +0300

    python: more assets' paths

--- cyphered/data/paths.py
+++ cyphered/data/paths.py
@@ -1,5 +1,10 @@
 import os
 
 RESOURCES_PATH = os.path.join(os.getcwd(), 'resources')
+SAVES_PATH = os.path.join(os.getcwd(), 'saves')
+DATA_PATH = os.path.join(os.getcwd(), 'data')
+
 FONTS_PATH = os.path.join(RESOURCES_PATH, 'fonts')
 SPRITES_PATH = IMAGES_PATH = os.path.join(RESOURCES_PATH, 'sprites')
+MUSIC_PATH = os.path.join(RESOURCES_PATH, 'music')
+
