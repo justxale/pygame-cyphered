0d02f041c307cf0494a2e11c99edfc49e348e62b
Xale <alexev2401@gmail.com>
2023-12-19 16:38:35 +0300

    python: base gameobject class

--- cyphered/objects/_base.py
+++ cyphered/objects/_base.py
@@ -1,2 +1,23 @@
-class BaseObject:
-    ...
+from ..data import Path
+import pygame
+
+
+class GameObject(pygame.sprite.Sprite):
+    def __init__(self, *groups):
+        super().__init__(*groups)
+        ...
+
+    def _load_image(self, filename, colorkey=None):
+        path = Path.sprite(filename)
+        image = pygame.image.load(path)
+        if colorkey is not None:
+            image = image.convert()
+            if colorkey == -1:
+                colorkey = image.get_at((0, 0))
+            image.set_colorkey(colorkey)
+        else:
+            image = image.convert_alpha()
+        self.image = image
+
+    def update(self, *args, **kwargs):
+        pass
