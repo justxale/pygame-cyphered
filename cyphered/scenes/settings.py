2b2cbb3f362e6f7e3b6c4dc362dba8f1ff13b05f
Natakass <na.kasatkina@1514.ru>
2023-12-22 21:19:22 +0300

    Настройки

--- cyphered/scenes/settings.py
+++ cyphered/scenes/settings.py
@@ -17,8 +17,9 @@ class SettingsScene(BaseScene):
             self.rightleft_button = Button("стрелки", step_x=300, font_size=25)
         elif Settings.move_keys == "ad":
             self.rightleft_button = Button("ad", step_x=300, font_size=25)
-        self.music_button_plus = Button("+", font_color=(255, 255, 255), step_x=-390, font_size=35)
-        self.music_button_minus = Button("-", font_color=(255, 255, 255), step_x=-430, font_size=35)
+        self.music_button_plus = Button("+", font_color=(255, 255, 255), step_x=-370, font_size=35)
+        self.music_button_minus = Button("-", font_color=(255, 255, 255), step_x=-440, font_size=35)
+        self.back = Button("Назад", font_color=(255, 255, 255), step_x=550, font_size=35, step_y=-250)
 
     def process_events(self, events):
         for event in events:
@@ -27,7 +28,17 @@ class SettingsScene(BaseScene):
                 if event.button == 1:
                     mouse_pos = pygame.mouse.get_pos()
                     if self.music_button_plus[2].collidepoint(mouse_pos):
-                        pass
+                        if Settings.music_volume + 0.1 <= 1:
+                            Settings.music_volume += 0.1
+                        else:
+                            Settings.music_volume = 1.0
+                    elif self.music_button_minus[2].collidepoint(mouse_pos):
+                        if Settings.music_volume - 0.1 >= 0:
+                            Settings.music_volume -= 0.1
+                        else:
+                            Settings.music_volume = 0.0
+                    Settings.music_volume = round(Settings.music_volume, 1)
+                    pygame.mixer.music.set_volume(Settings.music_volume)
                     if self.jump_button[2].collidepoint(mouse_pos):
                         if Settings.jump_key == "w":
                             Settings.jump_key = 'space'
@@ -43,6 +54,9 @@ class SettingsScene(BaseScene):
                             Settings.move_keys = "arrows"
                             self.rightleft_button = Button("стрелки", step_x=300, font_size=25)
                     Settings.save_settings()
+                    if self.back[2].collidepoint(mouse_pos):
+                        from .title import TitleScene
+                        self.switch_scene(TitleScene())
 
     def render(self, screen):
         # if music == 'on':
@@ -53,12 +67,19 @@ class SettingsScene(BaseScene):
         screen.blit(self.music_button_minus[0], self.music_button_minus[1])
         text_displ("Громкость музыки", screen, step_x=-390, step_y=-80, font_size=25,
                    font1="./resources/fonts/font.ttf")
+        a = Settings.music_volume
+        a = str(a)
+        text_displ(a, screen, step_x=-405, step_y=-30, font_size=25,
+                   font1="./resources/fonts/font.ttf")
+
         text_displ("Прыжок", screen, step_x=-160, step_y=-80, font_size=25,
                    font1="./resources/fonts/font.ttf")
         text_displ("Управление кнопками движения вправо-влево", screen, step_x=250, step_y=-80, font_size=25,
                    font1="./resources/fonts/font.ttf")
-
         pygame.draw.rect(screen, (0, 0, 0), self.jump_button[2])
         screen.blit(self.jump_button[0], self.jump_button[1])
+
         pygame.draw.rect(screen, (0, 0, 0), self.rightleft_button[2])
         screen.blit(self.rightleft_button[0], self.rightleft_button[1])
+        pygame.draw.rect(screen, (0, 0, 0), self.back[2])
+        screen.blit(self.back[0], self.back[1])
