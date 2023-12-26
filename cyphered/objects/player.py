7e3cbcfdb2bebd91800f9999363138105c8956b0
Xale <alexev2401@gmail.com>
2023-12-26 22:07:32 +0300

    basic player movement

--- cyphered/objects/player.py
+++ cyphered/objects/player.py
@@ -10,21 +10,55 @@ class Player(AnimatedGameObject):
         )
         self.rect.x = 200
         self.rect.y = 200
-        self.counter = 0
+
+        self.animation_counter = 0
+        self.is_facing_right = True
+        self.animation_buffer = self.is_facing_right
+
+        self.key_state = (False, 0, 0)
+
+    def play_next_animation_frame(self):
+        self.cur_frame = (self.cur_frame + 1) % (len(self.frames) // 2)
+        if self.is_facing_right:
+            self.image = self.frames[self.cur_frame]
+        else:
+            self.image = self.frames[self.cur_frame + 4]
+        self.animation_counter = 0
 
     def update(self, *args, **kwargs):
-        self.counter += 1
-        if self.counter >= 50:
-            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
-            self.image = self.frames[self.cur_frame]
-            self.counter = 0
-        move_x = kwargs.get('move_x')
-        move_y = kwargs.get('move_y')
+        move_x = kwargs.get('move_x', 0)
+        move_y = kwargs.get('move_y', 0)
+        if 'keydown' in args:
+            self.key_state = (True, move_x, move_y)
+        elif 'keyup' in args:
+            self.key_state = (False, 0, 0)
 
+        if self.key_state[0]:
+            if self.key_state[1]:
+                self.rect = self.rect.move(
+                    self.key_state[1], 0
+                )
+                if self.key_state[1] > 0:
+                    self.is_facing_right = True
+                else:
+                    self.is_facing_right = False
+            elif self.key_state[2]:
+                self.rect = self.rect.move(
+                    0, move_y
+                )
+
+        if self.animation_buffer != self.is_facing_right:
+            self.play_next_animation_frame()
+            self.animation_buffer = self.is_facing_right
+
+        self.animation_counter += 1
+        if self.animation_counter >= 25:
+            self.play_next_animation_frame()
+            self.animation_counter = 0
 
     def to_save_dict(self):
         return {
             'oid': 'player',
-            'x': self.x,
-            'y': self.y
+            'x': self.rect.x,
+            'y': self.rect.y
         }
--- cyphered/scenes/play.py
+++ cyphered/scenes/play.py
@@ -15,6 +15,25 @@ class PlayScene(BaseScene):
             'idle', 4, 2, all_sprites, player
         )
 
+    def process_events(self, events):
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                match event.key:
+                    case pygame.K_LEFT | pygame.K_a:
+                        player.update('keydown', move_x=-5)
+                    case pygame.K_RIGHT | pygame.K_d:
+                        player.update('keydown', move_x=5)
+                    case pygame.K_UP:
+                        player.update()
+            elif event.type == pygame.KEYUP:
+                match event.key:
+                    case pygame.K_LEFT | pygame.K_a:
+                        player.update('keyup')
+                    case pygame.K_RIGHT | pygame.K_d:
+                        player.update('keyup')
+                    case pygame.K_UP:
+                        player.update()
+
     def render(self, screen):
         all_sprites.draw(screen)
         all_sprites.update()
